9ae84c2266a9dffdcabad76154b1f08c1c6f1a53
intae <jjojjo141995@gmail.com>
2023-03-06 22:57:10 +0900

    example 제거

--- examples/factory/after/BlackShipFactory.ts
+++
@@ -1,9 +0,0 @@
-import { ShipFactory } from "./ShipFactory";
-import { BlackShip } from "./BlackShip";
-import { Ship } from "./Ship";
-
-export class BlackShipFactory extends ShipFactory {
-  createShip(): Ship {
-    return new BlackShip();
-  }
-}
--- examples/factory/after/Client.ts
+++
@@ -1,7 +0,0 @@
-import { ShipFactory } from "./ShipFactory";
-
-export class Client {
-  public static order(ship: ShipFactory, email: string) {
-    console.log(ship.orderShip(email).toString());
-  }
-}
